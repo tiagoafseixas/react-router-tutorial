32e8d6461fcf1f7270199d0a02afaf432ecfe989
tiagoafseixas <tiagoafseixas@gmail.com>
2022-08-03 07:23:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-nh5a8q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-nh5a8q)
---
+++ src/routes/invoices.jsx
@@ -0,0 +1,35 @@
+import { NavLink, Outlet, Link } from 'react-router-dom';
+import { getInvoices } from '../data';
+
+
+
+export default function Invoices() {
+  let invoices = getInvoices();
+  return (
+    <div style={{ display: 'flex' }}>
+      <nav
+        style={{
+          borderRight: 'solid 1px',
+          padding: '1rem',
+        }}
+      >
+        {invoices.map((invoice) => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: 'block',
+                margin: '1rem 0',
+                color: isActive ? 'red' : '',
+              };
+            }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
